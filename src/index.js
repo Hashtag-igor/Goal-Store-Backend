504a5edea0961d6c113bc4bff3dd4a31eb80ba7d
Igor Perdigão <77364859+Hashtag-igor@users.noreply.github.com>
2023-10-28 22:06:20 -0300

    Criei o arquivo vercel.json e fiz alterações no index.js com intuito de solucionar o problema de hospedagem do servidor no vercel

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
-// src/index.js
 import express from "express";
 import cors from "cors";
+import { fileURLToPath } from 'url';
+import path from "path";
 import ConnectDatabase from "./database/db.js";
 import routes from "./routes.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const port = process.env.PORT || 3001;
 const app = express();
@@ -11,6 +15,12 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
+// Rota para o favicon.ico
+const faviconPath = path.join(__dirname, "../public", "favicon.ico");
+app.get("/favicon.ico", (req, res) => {
+  res.sendFile(faviconPath);
+});
+
 ConnectDatabase()
   .then(() => {
     app.listen(port, () => console.log("Banco de dados Conectado"));
@@ -18,6 +28,4 @@ ConnectDatabase()
   .catch((error) => {
     console.log(error);
   });
-
-export default app;
 
